1c8f3cce6ba5b11a16a525dfc54895832b9ed595
nina faure beaulieu <nina-fb@outlook.com>
2022-04-05 13:43:09 +0200

    formatting changes

--- scripts_planning/iucnmaps.R
+++ scripts_planning/iucnmaps.R
@@ -27,7 +27,6 @@ files = list.files(path = "wildoceans-scripts/IUCN/Sharks_rays_SA_raw",pattern =
 names = toupper(files)
 names = str_split(names,toupper("wildoceans-scripts/IUCN/Sharks_rays_SA_raw/"), simplify = TRUE)[,2]
 names = str_split(names,".GPKG", simplify = TRUE)[,1]
-names = names[-21]
 # all iucn maps
 iucn_stack_all = stack()
 for(i in 1:length(files)){
@@ -35,7 +34,6 @@ for(i in 1:length(files)){
   temp = fasterize(temp,pu)
   iucn_stack_all = addLayer(iucn_stack_all,temp)
 }
-iucn_stack_all = dropLayer(iucn_stack_all,21)
 # add names to rasterstack
 names(iucn_stack_all) = names
 
@@ -50,7 +48,7 @@ files = files[names %in% featurenames$SPECIES_SCIENTIFIC]
 names = names[names %in% featurenames$SPECIES_SCIENTIFIC]
 
 # not all species have an IUCN range map 
-# currently missing for Etmopterus granulosus
+# currently missing for Etmopterus granulosus, DEANIA CALCEUS is DEANIA CALCEA, HIMANTURA LEOPARDA
 featurenames$SPECIES_SCIENTIFIC[!(featurenames$SPECIES_SCIENTIFIC %in% names)]
 
 # create raster stack of iucn range maps (only for species with modelled data)
@@ -67,4 +65,7 @@ filtered = featurenames %>%
   filter(MODELTYPE == "ASEASONAL")
 names(iucn_stack) = filtered$FEATURENAME
 plot(iucn_stack)
+# remove CARCHARHINUS_AMBLYRHYNCHOS
+iucn_stack = dropLayer(iucn_stack,13)
+
 # ---------------------------------
