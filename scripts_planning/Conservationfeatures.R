a4aeac9dcaef3a796ae9695cc445efbf0e081fd7
nina faure beaulieu <nina-fb@outlook.com>
2022-04-05 13:45:21 +0200

    formatting changes and added possibility to have a binary maps raster stack (binary stack made using threshold values)

--- scripts_planning/Conservationfeatures.R
+++ scripts_planning/Conservationfeatures.R
@@ -1,9 +1,17 @@
+# ---------------------------------------------------------------------------------
+# AUTHORS: Nina Faure Beaulieu, Dr. Victoria Goodall (2021)
+# PROJECT: Shark and ray protection project, WILDOCEANS a programme of the WILDLANDS CONSERVATION TRUST
+# CONTACTs: ninab@wildtrust.co.za; victoria.goodall@mandela.ac.za 
+# ---------------------------------------------------------------------------------
+
+
 # ---------------------------------
 # DATA
 # ---------------------------------
-
 # species distribution file names (continuous)
 files = list.files(path = paste0(getwd(),"/wildoceans-scripts/"),pattern = "ensemblemean.tif", recursive = TRUE,full.names = TRUE)
+
+# create raster stack
 feature_stack = stack()
 for(i in 1:length(files)){
   temp = raster(files[i])
@@ -12,33 +20,23 @@ for(i in 1:length(files)){
 }
 rm(i,files,temp) # remove unnecessary variables
 
-# species distribution file names (binary)
-#files = list.files(path = paste0(path,"Dropbox/6-WILDOCEANS/Modelling/Outputs"),pattern = "ensemblemeanthreshold.tif", recursive = TRUE,full.names = TRUE)
-#feature_stack_binary = stack()
-#for(i in 1:length(files)){
-#  temp = raster(files[i])
-#  temp = projectRaster(temp,pu)
-#  feature_stack_binary = addLayer(feature_stack_binary,temp)
-#}
-#rm(i,files,temp) # remove unnecessary variables
+# threshold values (to turn continuous distributions to binary)
+threshs = list.files(path = paste0(path,"Dropbox/6-WILDOCEANS/wildoceans-scripts/"),pattern = "thresh.csv", recursive = TRUE, full.names = TRUE)
+# ---------------------------------
+
 
 # ---------------------------------
 # FORMATTING
 # ---------------------------------
-
-# turn all NA values to 0
+# turn all NA values to 0 (prioritizr does not like NA values)
 values(feature_stack)[is.na(values(feature_stack))] = 0
-#values(feature_stack_binary)[is.na(values(feature_stack_binary))] = 0
 
 # mask with pu
 feature_stack = stack(mask(feature_stack,pu))
-#feature_stack_binary = stack(mask(feature_stack_binary,pu))
 
 # extract scientific name from stack of distributions
 featurenames = as.data.frame(names(feature_stack))
-#featurenamesbinary = as.data.frame(names(feature_stack_binary))
 colnames(featurenames) = "featurename"
-#colnames(featurenamesbinary) = "featurename_binary"
 for(i in 1:nrow(featurenames)){
   # extract model type
   featurenames$modeltype[i] = strsplit(featurenames$featurename,"_")[[i]][3]
@@ -54,11 +52,7 @@ featurenames$species_scientific = toupper(featurenames$species_scientific)
 # turn headers to capital
 colnames(featurenames) = toupper(colnames(featurenames))
 
-# add binary stack names
-#featurenames = cbind(featurenames,featurenamesbinary)
-#rm(featurenamesbinary)
-
-# turn season type to upper case
+# turn model type (season) to upper case
 featurenames$MODELTYPE = toupper(featurenames$MODELTYPE)
 
 # ---------------------------------
@@ -82,61 +76,33 @@ feature_stack = stack(feature_stack)
 
 rm(i) # remove unnecessary variables
 
-# create raster stack using threshold values
-#colnames(thresholds_df) = toupper(colnames(thresholds_df))
-#featurenames = left_join(featurenames,thresholds_df)
-#rm(thresholds_df)
+# create feature stack where threshold values are used to filter low probability portion of occurences
+feature_stack_thresholds = stack()
+for(i in 1:nlayers(feature_stack)){
+  temp = feature_stack[[i]]
+  thresh_value = read.csv(threshs[i])
+  thresh_value = (thresh_value$thresh)/1000
+  values(temp)[values(temp)<thresh_value] = 0
+  feature_stack_thresholds = addLayer(feature_stack_thresholds,temp)
+}
+rm(temp)
 
-#feature_stack_binary = feature_stack
-#for(i in 1:nlayers(feature_stack_binary)){
-#  feature_stack_binary[[i]][values(feature_stack_binary[[i]])<featurenames$THRESH[i]] = 0
-#  feature_stack_binary[[i]][values(feature_stack_binary[[i]])>featurenames$THRESH[i]] = 1
-#  }
-
-# filter feature stacks by modeltype
-# filter feature stack as well
-# this creates a separate raster stack for each problem run
-keep = str_detect(names(feature_stack),"Aseasonal")  # identify Aseasonal layers in stack
+# filter to only keep aseasonal features
+keep = str_detect(names(feature_stack),"Aseasonal")  # identify aseasonal layers in stack
 idx = which(keep == FALSE) # find out layer number with feature to omit
 feature_stack_aseasonal = dropLayer(feature_stack,idx)
 rm(idx,keep) # remove
 
+# filter to only keep aseasonal features of threshold stack
+keep = str_detect(names(feature_stack_thresholds),"Aseasonal")  # identify aseasonal layers in stack
+idx = which(keep == FALSE) # find out layer number with feature to omit
+feature_stack_aseasonal_thresholds = dropLayer(feature_stack_thresholds,idx)
+rm(idx,keep) # remove
 
-#keep = str_detect(names(feature_stack_binary),"Aseasonal")  # identify Aseasonal layers in stack
-#idx = which(keep == FALSE) # find out layer number with feature to omit
-#feature_stack_binary_aseasonal = dropLayer(feature_stack_binary,idx)
-#rm(idx,keep) # remove
+# remove redundant feature stacks
+rm(feature_stack,feature_stack_thresholds,i,idx, thresh_value,threshs)
 
-#keep = str_detect(names(feature_stack),c("summer")) 
-#idx = which(keep == FALSE) # find out layer number with feature to omit
-#feature_stack_summer = dropLayer(feature_stack,idx)
-#rm(idx,keep) # remove
-
-#keep = str_detect(names(feature_stack),c("winter")) 
-#idx = which(keep == FALSE) # find out layer number with feature to omit
-#feature_stack_winter = dropLayer(feature_stack,idx)
-#rm(idx,keep) # remove
-
-
-
-# species distribution threshold values
-#thresholds = list.files(path = paste0(path,"Dropbox/6-WILDOCEANS/Modelling/Outputs"),pattern = "thresh.csv", recursive = TRUE,full.names = TRUE)
-#thresholds_df = data.frame()
-#for(i in 1:length(thresholds)){
-#  name = str_split(thresholds[i],"/")[[1]][9]
-# extract parameters from file name (very clunky for now)
-#  model =  str_split(name," ")[[1]][2]
-#  genus = str_split(name," ")[[1]][3]
-#  species = str_split(name," ")[[1]][4]
-# read csv
-#  thresh = read.csv(thresholds[i])
-#  thresh$X = NULL
-# add parameters
-#  thresh$modeltype = model
-#  thresh$species_scientific = paste(genus, species)
-# divide value by 1000
-#  thresh$thresh = thresh$thresh/1000
-# add to maindataframe
-#  thresholds_df = rbind(thresholds_df,thresh)
-#  }
-#rm(name,model,genus,species,thresh,thresholds)
+# add endemism and iucn status to feature names
+master_temp = master[,c("SPECIES_SCIENTIFIC","ENDEMIC.STATUS","STATUS")]
+featurenames = left_join(featurenames,master_temp)
+rm(master_temp)
